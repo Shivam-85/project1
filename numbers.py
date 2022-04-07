675f9c8966c8dfe4cd50c973b2ee6a8b01935394
user <user@users-MacBook-Air-2.local>
2022-04-07 15:07:37 +0530

    numbers completed

---
+++ numbers.py
@@ -0,0 +1,22 @@
+# Addition
+print(3 + 4)
+
+# Subtraction
+print(4 - 2)
+
+# divison
+print(5 / 2)
+
+# floor division
+print(5 // 2)
+
+# power
+print(2 ** 3)
+
+# square root
+print(2 ** 0.5)
+
+# modulo
+print(4 % 3)
+
+
