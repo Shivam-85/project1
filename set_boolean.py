c600fef31e87f88c80c15076b3a7354c1c3d29ec
user <user@users-MacBook-Air-2.local>
2022-04-11 10:45:45 +0530

    Set and Boolean completed

---
+++ set_boolean.py
@@ -0,0 +1,26 @@
+# set
+s = set()
+
+s.add(1)
+s.add(2)
+s.add(2)
+
+print(len(s))
+print(type(s))
+print(s)
+
+list1 = [1, 2, 3, 1, 3, 4, 6, 7, 3, 2, 5]
+
+s1 = set(list1)
+
+print(s1)
+
+# boolean
+
+b = True
+print(b)
+
+print(1 > 2)
+
+b = None
+print(b)
