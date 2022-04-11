dfa1f67302df3e52b7ff184fead1c940718638b7
user <user@users-MacBook-Air-2.local>
2022-04-11 10:33:51 +0530

    tuple completed

---
+++ tuples.py
@@ -0,0 +1,20 @@
+t = (1, 2, 3, 1)
+
+print(len(t))
+
+print(type(t))
+
+print(t[0])
+
+print(t[-1])
+
+print(t.index(1))
+
+print(t.count(1))
+
+# immutability
+
+t[0] = "Shivam"  # gives error
+
+
+
